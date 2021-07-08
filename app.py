b4e3a4ba658a24eebee45cdf8a91068e4013521d
Kunjal9 <86723072+Kunjal9@users.noreply.github.com>
2021-07-08 15:48:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/query', methods=['POST'])
 def get_bot_response():
     try:
         res="Hello"
-
         return jsonify({"OK":"true","data":{"message":res}}),200
 
     except Exception as e:
         return jsonify({"OK":"false","data":{"error":str(e)}}),400
 
 
-
 if __name__ == '__main__':
-  
     app.run(debug=True) 
 
 
