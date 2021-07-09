cd04216d29579263f0016978e2372f2ccf22ae4e
Nisarg Soni <nisarg.soni@smartsensesolutions.com>
2021-07-09 09:51:51 +0530

    flask api initial connect with rasa

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 from flask import Flask, request, jsonify
-
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/query', methods=['POST'])
 def get_bot_response():
+    """
+    this function is fetching query from the request body and then will get response from 
+    rasa chatbot
+
+    [ get query variable from the request body ]
+
+    Returns:
+        [json]: [response from the rasa]
+    """
     try:
-        res="Hello"
-        return jsonify({"OK":"true","data":{"message":res}}),200
+        data=request.get_json()
+        query=data.get('query',None)
+
+        if query==None or query==' ':
+            raise Exception('Please provide query to chatbot')
+
+        response=requests.post('http://localhost:5005/webhooks/rest/webhook',json={"message":query})    
+        res=response.json()
+        res=res[0]['text']
+
+        return jsonify({"data":{"message":res}}),200
 
     except Exception as e:
-        return jsonify({"OK":"false","data":{"error":str(e)}}),400
-
+        return jsonify({"data":{"error":str(e)}}),400
 
 if __name__ == '__main__':
     app.run(debug=True) 
-
-
